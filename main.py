db57a0de30f1132826791cde70e7e0dc793c140b
dudgeon <dudgeon@gmail.com>
2023-03-31 15:04:49 +0000

    Added user context to prompt

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ app = Flask(__name__)
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-def generate_text(prompt):
+def generate_text(prompt, context=None):
     model_engine = "text-davinci-002"
-    prompt = f"{prompt}{{{{response}}}}"
+    full_prompt = f"{context} {prompt}{{{{response}}}}" if context else f"{prompt}{{{{response}}}}"
+    
+    # Calculate tokens in the input prompt
+    input_tokens = len(full_prompt.split())
+    
+    # Set max_tokens based on the input_tokens
+    max_response_tokens = max(150, min(input_tokens * 5, 4096))
+
     completions = openai.Completion.create(
         engine=model_engine,
-        prompt=prompt,
-        max_tokens=150,
+        prompt=full_prompt,
+        max_tokens=max_response_tokens,
         n=1,
         stop=None,
         temperature=0.7,
@@ -27,7 +34,8 @@ def home():
 @app.route("/generate_async", methods=["POST"])
 def generate_async():
     input_text = request.form["input_text"]
-    generated_text = generate_text(input_text)
+    context = request.form["context"]
+    generated_text = generate_text(input_text, context)
     return jsonify({"result": generated_text})
 
 app.run(host="0.0.0.0", port=8080)
